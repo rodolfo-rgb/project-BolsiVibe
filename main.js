5ccdf9ec42039c8a53b574d083d49f5923f5ec59
RodoAL <rodo.aparicio.lopez@gmail.com>
2025-01-04 22:18:55 -0600

    Add Tailwind CSS and Electron setup with initial configuration files

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            enableRemoteModule: false,
+            nodeIntegration: false,
+        },
+    });
+
+    win.loadURL('http://localhost:5173'); // Cambia esto si tu puerto es diferente
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
